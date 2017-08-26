60fb84795d167c1c59b8a7f2a969f25e927382ef
ton4o_1 <ton4o_1@yahoo.com>
2017-08-26 19:59:25 +0300

    edits

--- ClickOut/app/src/main/java/com/clickout/clickout/MainActivity.java
+++ ClickOut/app/src/main/java/com/clickout/clickout/MainActivity.java
@@ -1,9 +1,9 @@
 package com.clickout.clickout;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Typeface;
-import android.media.Image;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -11,8 +11,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,9 +31,19 @@ public class MainActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
+
         Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/ubuntu_b.ttf");
         TextView game_title = (TextView)findViewById(R.id.game_title);
         game_title.setTypeface(custom_font);
+
+        //animated layout
+        Context c = getBaseContext();
+        Animation scale = AnimationUtils.loadAnimation(c, R.anim.scale);
+        scale.setInterpolator(new LinearInterpolator());
+        Animation rotate = AnimationUtils.loadAnimation(c, R.anim.rotate);
+        rotate.setInterpolator(new LinearInterpolator());
+        LinearLayout spl = (LinearLayout) this.findViewById(R.id.spl);
+        spl.startAnimation(scale);
 
         ImageButton btnSinglePlayer = (ImageButton) this.findViewById(R.id.btn_single_player);
 
@@ -50,8 +63,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
         ImageButton btnSettings = (ImageButton) findViewById(R.id.btn_settings);
+        ImageButton btnStats = (ImageButton) findViewById(R.id.btn_stats);
+        ImageButton btnExit = (ImageButton) findViewById(R.id.btn_exit);
+
+        btnSettings.startAnimation(rotate);
+        btnStats.startAnimation(rotate);
+        btnExit.startAnimation(rotate);
+
         btnSettings.setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View view) {
@@ -59,10 +78,7 @@ public class MainActivity extends AppCompatActivity {
              }
         });
 
-
-
-        ImageButton exit = (ImageButton) findViewById(R.id.btn_exit);
-        exit.setOnClickListener(new View.OnClickListener()
+        btnExit.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
@@ -75,8 +91,27 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
-
     }
+
+ // public void onResume(){
+ //     super.onResume();
+//
+ // int x = ScreenUtil.getScreenWidth(this.getWindowManager())+450;
+ //   int y = ScreenUtil.getScreenHeight(this.getWindowManager())+450;
+//
+ //   final ParticleSystem ps1 = new ParticleSystem(this, 50000, R.drawable.ic_confetti_one,10000, R.id.emiter);
+ //       ps1.setSpeedModuleAndAngleRange(0.3f, 0.3f, 70, 70);
+ //       ps1.setAcceleration(0.00025f,180);
+ //       ps1.setScaleRange(0.5f, 1.5f);
+ //       ps1.emit(x,-450,1);
+//
+ //   final ParticleSystem ps4 = new ParticleSystem(this, 50000, R.drawable.ic_confetti_two,10000, R.id.emiter);
+ //       ps4.setSpeedModuleAndAngleRange(0.3f, 0.3f, 250, 250);
+ //       ps4.setAcceleration(0.00025f,0);
+ //       ps4.setScaleRange(0.5f, 1.5f);
+ //       ps4.emit(-450,y,1);
+//
+ // }
 
     private void showSelectModeDialog() {
         AlertDialog.Builder adb = new AlertDialog.Builder(this);
